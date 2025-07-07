c7c03cf757c556ad9704f6c1e42554c78fc3585c
Anjel28 <anjelchaudhary28@gmail.com>
2025-07-07 23:24:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return (
+    <>
+    
+    <Navbar tittle="Textutills"  aboutText="About Textutills"/> 
+  
+    
+    </>
+  )
+}
+
+export default App
